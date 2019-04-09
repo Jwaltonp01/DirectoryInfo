86e9729bc1f9fc598b6d92e75ac1769e3e56830e
ウォルトン・プラッチャー・ジェリー <jwaltonp@reas.jp>
2019-04-09 15:37:29 +0900

    # START #	Start

---
+++ directory_info.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+import os
+
+# Project Root Directory
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+# Project Name
+PROJECT_DIR_NAME = "ENTER_PROJECT_NAME"
+
+
+def get_root_dir():
+    """
+    Returns the project root directory
+
+    :return:
+        Project path
+    """
+    global ROOT_DIR
+
+    paths = ROOT_DIR.split("/")
+    paths = list(filter(lambda a: a != '', paths))
+
+    if PROJECT_DIR_NAME not in paths[-1]:
+        ROOT_DIR = ROOT_DIR + PROJECT_DIR_NAME
+
+    if ROOT_DIR[-1] != "/":
+        ROOT_DIR = ROOT_DIR + "/"
+
+    return ROOT_DIR
